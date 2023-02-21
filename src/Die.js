988fbca84eb50978d2ddb977c76446d8b0d0318e
Iran <iransaboonian@gmail.com>
2023-02-21 23:29:12 +0330

    tenzi game

--- src/Die.js
+++ src/Die.js
@@ -1,10 +1,11 @@
-import React from "react"
 
-export default function Die(props){
-  return(
-    <div onClick={props.holdDice}>
-  <h1 className={`die-num ${props.isHeld?"held":""}` }
-  >{props.value}</h1>
-  </div>
-  )
+import React from "react" 
+ 
+export default function Die(props){ 
+  return( 
+    <div onClick={props.holdDice}> 
+  <h1 className={`die-num ${props.isHeld?"held":""}` } 
+  >{props.value}</h1> 
+  </div> 
+  ) 
   }
